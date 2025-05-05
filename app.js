51e59c9a8a64619be169fa44192b6206a2295f2e
antonicmab <antonicmab@gmail.com>
2025-05-05 23:51:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ Telegram.WebApp.onEvent("mainButtonClicked", function() {
 });
 
 // Отображаем имя пользователя
-// let usercard = document.getElementById("usercard");
-// let p = document.createElement("p");
-// p.innerText = `${tg.initDataUnsafe.user.first_name ?? ''} ${tg.initDataUnsafe.user.last_name ?? ''}`;
-// usercard.appendChild(p);
+let usercard = document.getElementById("usercard");
+let p = document.createElement("p");
+p.innerText = `${tg.initDataUnsafe.user.first_name ?? ''} ${tg.initDataUnsafe.user.last_name ?? ''}`;
+usercard.appendChild(p);
 
 
